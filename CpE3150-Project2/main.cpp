3ea2989fc0ba9ef1b14cf950a669c663c7ed9af5
Mitchell Skaggs <skaggsm333@gmail.com>
2019-12-02 14:09:22 -0600

    Initial commit

---
+++ CpE3150-Project2/main.cpp
@@ -0,0 +1,18 @@
+/*
+ * CpE3150-Project2.cpp
+ *
+ * Created: 12/2/2019 1:59:18 PM
+ * Author : Mitchell
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
---
+++ README.md
@@ -0,0 +1,5 @@
+# CpE 3150, Project 2
+
+Team 1: Logan DeVasure, Sam Short, Mitchell Skaggs, Zachary Wuller
+
+This is the repository for our Project 2. Open in AtmelStudio >= 7.
