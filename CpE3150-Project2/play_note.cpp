47dd06256ba98297507a539980ff25903a8a4310
sam <snspzv@mst.edu>
2019-12-09 13:25:25 -0600

    Added delay function to play_note

--- CpE3150-Project2/play_note.cpp
+++ CpE3150-Project2/play_note.cpp
@@ -37,9 +37,24 @@ void play_note(float freq, float counts, int bpm)
 	
 	TCCR1B = 0x00; // turn off
 	TCCR3B = 0x00; // turn off
+	
+	delay(bpm); //allow for time between notes played
 }
 
 ISR(TIMER1_COMPA_vect)
 {
 	PORTE ^= (1 << PORTE4); // toggle speaker
+}
+
+void delay(int bpm)
+{
+	TCCR3A = 0x00;
+	TCCR3B = (1 << WGM32) | (1 << CS32); // CTC mode, 256 prescale
+	OCR3A = ((1 / (bpm / 60)) * 0.25) / 0.000016; //bpm / 60 sec = bps, 1/bps = seconds per count, delay will be 1/4 of a count
+	
+	while(!(TIFR3 & (1 << OCF3A)));
+
+	TIFR3 |= (1 << OCF3A); 
+	
+	TCCR3B = 0x00; // turn off
 }
