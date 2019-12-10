b26da5f5ee4c7ba0d99462a0e0fea7338c2547aa
Wuller <zwwwd9@umsystem.edu>
2019-12-09 19:07:08 -0600

    keyboard.cpp update

--- CpE3150-Project2/keyboard.cpp
+++ CpE3150-Project2/keyboard.cpp
@@ -1,41 +1,77 @@
 #include "keyboard.h"
 #include "note.h"
 #include "play_note.h"
+#include "led.h"
 using namespace std;
 
 void keyboard()
 {
 	DDRA = 0b00000000;
 	PORTA = 0b11111111;
-	DDRD = 0b11111111;
 	
 	char note[3];
 	
 	if(~PINA & (1<<PINA0)) // switch 1
 	{
-		note[] = {'C','4'};
-		note->note(note[]);
-		play_note(note->get_frequency(), 4)
-		PORTD = 0b11111110;
+		note[] = {'C','5'};
+		note(note[]);
+		play_note(get_frequency(), 4)
+		toggle_led(get_note_letter());
 		while(~PINA & (1<<PINA0)); //avoid debouncing
 	}
 	
 	if(~PINA & (1<<PINA1)) // switch 2
 	{
-		note[] = {'D','4'};
-		note->note(note[]);
-		play_note(note->get_frequency(), 4);
-		PORTD = 0b11111101;
+		note[] = {'D','5'};
+		note(note[]);
+		play_note(get_frequency(), 4);
+		toggle_led(get_note_letter());
 		while(~PINA & (1<<PINA1)); //avoid debouncing
 	}
 	
 	if(~PINA & (1<<PINA2)) //switch 3
 	{
-		note[] = {'E','4'};
-		note->note(note[]);
-		play_note(note->get_frequency(), 4);
-		PORTD = 0b11111011;
+		note[] = {'E','5'};
+		note(note[]);
+		play_note(get_frequency(), 4);
+		toggle_led(get_note_letter());
 		while(~PINA & (1<<PINA2)); //avoid debouncing
+	}
+	
+	if(!PINA & (1<<PINA3)) //switch 4
+	{
+		note[] = {'F','5'};
+		note(note[]);
+		play_note(get_frequency(), 4);
+		toggle_led(get_note_letter());
+		while(~PINA & (1<<PINA3)); //avoid debouncing
+	}
+	
+	if(~PINA & (1<<PINE6)) //switch 5
+	{
+		note[] = {'G','5'};
+		note(note[]);
+		play_note(get_frequency(), 4);
+		toggle_led(get_note_letter());
+		while(~PINA & (1<<PINE6)); // avoid debouncing
+	}
+	
+	if(~PINA & (1<<PINA4)) //switch 6
+	{
+		note[] = {'A','5'};
+		note(note[]);
+		play_note(get_frequency(), 4);
+		toggle_led(get_note_letter());
+		while(~PINA & (1<<PINA4)); // avoid debouncing
+	}
+	
+	if(~PINA & (1<<PINA5)) //switch 7
+	{
+		note[] = {'B','5'};
+		note(note[]);
+		play_note(get_frequency(), 4);
+		toggle_led(get_note_letter());
+		while(~PINA & (1<<PINA5)); //avoid debouncing
 	}
 	return 0;
 }
