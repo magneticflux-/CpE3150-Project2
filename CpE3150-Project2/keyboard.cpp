95b894fdc5021e6797c857ab49c48665479f6e56
Mitchell Skaggs <skaggsm333@gmail.com>
2019-12-09 20:30:01 -0600

    Reformat

--- CpE3150-Project2/keyboard.cpp
+++ CpE3150-Project2/keyboard.cpp
@@ -3,6 +3,8 @@
 #include "play_note.h"
 #include "led.h"
 
+#define HELD_LENGTH 2
+
 void init_keyboard()
 {
 	DDRA = 0b00000000;
@@ -11,43 +13,43 @@ void init_keyboard()
 
 void check_keyboard()
 {
-	if(~PINA & (1<<PINA0)) // switch 1
+	if(~PINA & (1 << PINA0)) // switch 1
 	{
-		play_note("C5", 4);
+		play_note("C5", HELD_LENGTH);
 	}
 	
-	if(~PINA & (1<<PINA1)) // switch 2
+	if(~PINA & (1 << PINA1)) // switch 2
 	{
-		play_note("D5", 4);
+		play_note("D5", HELD_LENGTH);
 	}
 	
-	if(~PINA & (1<<PINA2)) //switch 3
+	if(~PINA & (1 << PINA2)) //switch 3
 	{
-		play_note("E5", 4);
+		play_note("E5", HELD_LENGTH);
 	}
 	
-	if(~PINA & (1<<PINA3)) //switch 4
+	if(~PINA & (1 << PINA3)) //switch 4
 	{
-		play_note("F5", 4);
+		play_note("F5", HELD_LENGTH);
 	}
 	
-	if(~PINA & (1<<PINA4)) //switch 5
+	if(~PINA & (1 << PINA4)) //switch 5
 	{
-		play_note("G5", 4);
+		play_note("G5", HELD_LENGTH);
 	}
 	
-	if(~PINA & (1<<PINA5)) //switch 6
+	if(~PINA & (1 << PINA5)) //switch 6
 	{
-		play_note("A5", 4);
+		play_note("A5", HELD_LENGTH);
 	}
 	
-	if(~PINA & (1<<PINA6)) //switch 7
+	if(~PINA & (1 << PINA6)) //switch 7
 	{
-		play_note("B5", 4);
+		play_note("B5", HELD_LENGTH);
 	}
 
-	if(~PINA & (1<<PINA7)) //switch 8
+	if(~PINA & (1 << PINA7)) //switch 8
 	{
-		play_note("C6", 4);
+		play_note("C6", HELD_LENGTH);
 	}
 }
